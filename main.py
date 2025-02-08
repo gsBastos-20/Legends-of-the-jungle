056586de032216b8974e56b44deca9f5a609cb3c
Mika <kakamarques.0202@gmail.com>
2025-02-08 15:03:08 -0300

    bug sound

--- main.py
+++ main.py
@@ -31,15 +31,12 @@ mensagem = "Pressione z para começar"
 fonte_menu = pygame.font.SysFont('Pixeled', 50, True, False)
 start_formatado = fonte_menu.render(mensagem, False, (160, 42, 45))
 
-#config. musica menu
+#musicas
 pygame.mixer.music.set_volume(0.4)
 musica_menu = pygame.mixer.music.load("musicas\\overworld-day.mp3")
-pygame.mixer.music.play(-1)
-tocando_menu = True
-def sair_menu():
-    if tocando_menu == False:
-        pygame.mixer.music.pause()
-
+musica_menu = pygame.mixer.music.play(-1)
+musica_game = pygame.mixer.music.load("musicas\\jungle.mp3")
+musica_game = pygame.mixer.music.play(-1)
 
 rodando = True
 while rodando:
@@ -54,12 +51,11 @@ while rodando:
                 exit()
             if event.type == KEYDOWN:
                 if event.key == K_z:
-                    tocando_menu = False
-                    sair_menu()
                     menu = False
         pygame.display.flip()
     
 
+    
     relogio.tick(60)
     tela.blit(fundo_game, (0, 0))
     for event in pygame.event.get():
